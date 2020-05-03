e8c2e4183424d3b5d606c1bb6608f5afd29c4645
Banee Ishaque K <Baneeishaque@gmail.com>
2020-05-03 15:46:10 +0530

    BFG

--- files_of_a_directory_recursive.bash
+++ files_of_a_directory_recursive.bash
@@ -5,13 +5,14 @@ do
 #  echo $i
 	if [ -f "$i" ];
     then
+		printf "File: %s\n" "${i}"
         # printf "Path: %s\n" "${i%/*}" # shortest suffix removal
-        java -jar /C/Programs/bfg-1.13.0.jar --delete-files "${i##*/}" # longest prefix removal
+        # java -jar /C/Programs/bfg-1.13.0.jar --delete-files "${i##*/}" # longest prefix removal
         # printf "Extension: %s\n"  "${i##*.}"
         # printf "Filesize: %s\n" "$(du -b "$i" | awk '{print $1}')"
         # some other command can go here
         # printf "\n\n"
-		git reflog expire --expire=now --all
-		git gc --prune=now --aggressive
+		# git reflog expire --expire=now --all
+		# git gc --prune=now --aggressive
     fi
 done
