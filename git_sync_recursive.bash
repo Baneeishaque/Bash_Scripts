d38651f35bdbc408aec9eed4bc2dd928d45ff4d2
Banee Ishaque K <baneeishaque@gmail.com>
2020-05-04 13:37:34 +0530

    Create git_sync_recursive.bash

---
+++ git_sync_recursive.bash
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# Skips directories that contain a file called .ignore
+
+HIGHLIGHT="\e[01;34m"
+NORMAL='\e[00m'
+
+function update {
+  local d="$1"
+  if [ -d "$d" ]; then
+    #echo "Looking for $d/.ignore"
+    if [ -e "$d/.ignore" ]; then
+      printf "%b\n" "\n${HIGHLIGHT}Ignoring $d${NORMAL}"
+    else
+      cd "$d" > /dev/null
+      if [ -d ".git" ]; then
+        printf "%b\n" "\n${HIGHLIGHT}Updating `pwd`$NORMAL"
+        git pull
+		# if own repository, push
+		# git push
+      elif [ ! -d .svn ] && [ ! -d CVS ]; then
+        scan *
+      fi
+      cd .. > /dev/null
+    fi
+  fi
+  #echo "Exiting update: pwd=`pwd`"
+}
+
+function scan {
+  #echo "`pwd`"
+  #echo "About to scan $*"
+  for x in $*; do
+    update "$x"
+  done
+}
+
+function updater {
+  if [ "$1" != "" ]; then cd "$1" > /dev/null; fi
+  printf "%b\n" "${HIGHLIGHT}Scanning ${PWD}${NORMAL}"
+  scan *
+}
+
+if [ "$1" == "" ]; then
+  updater
+else
+  for dir in "$@"; do
+    updater "$dir"
+  done
+fi
