b514bcbe4cbd62ac4ae4eb4b89229a31a2be7f34
Banee Ishaque K <baneeishaque@gmail.com>
2022-05-03 14:50:03 +0530

    More Scripts

--- activate_asdf.sh
+++ activate_asdf.sh
@@ -1,5 +1,2 @@
-. /opt/activateHomeBrew.sh
-export ASDF_DART_ENABLE_BETA=true
-export ASDF_DART_ENABLE_DEV=true
-export PKG_CONFIG_PATH=/usr/share/pkgconfig:/usr/lib/x86_64-linux-gnu/pkgconfig
-. /home/linuxbrew/.linuxbrew/opt/asdf/libexec/asdf.sh
+. $HOME/.asdf/asdf.sh
+. $HOME/.asdf/completions/asdf.bash
--- installUpdates.sh
+++ installUpdates.sh
@@ -1,1 +1,1 @@
-sudo apt-get update && sudo apt-get upgrade && sudo apt-get full-upgrade && sudo apt-get autoremove
+sudo apt-get update ; sudo apt-get upgrade && sudo apt-get full-upgrade && sudo apt-get autoremove
