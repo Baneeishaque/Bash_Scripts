f200ba073e2cec002f4feb93e9b2fcec4f3d2d6b
Banee Ishaque K <baneeishaque@gmail.com>
2022-06-13 23:51:17 +0530

    Rclone

--- mirrorLocalToOnlineDrives.bash
+++ mirrorLocalToOnlineDrives.bash
@@ -1,19 +1,57 @@
 cd ~
+
+rclone sync Data/Misc/Blomp-Banee-Gmail-Drive/ Blomp-Banee-Gmail-Drive: --progress --progress-terminal-title --human-readable --filter-from Data/Labs/Lab_Banee/Misc/configurations-private/rclone/Blomp-Banee-Gmail-Drive-filter.txt --dry-run
+
+rclone sync Data/Misc/Blomp-Banee2-Gmail-Drive/ Blomp-Banee2-Gmail-Drive: --progress --progress-terminal-title --human-readable --filter-from Data/Labs/Lab_Banee/Misc/configurations-private/rclone/Blomp-Banee2-Gmail-Drive-filter.txt --dry-run
+
+rclone sync Data/Misc/Blomp-Banee3-Gmail-Drive/ Blomp-Banee3-Gmail-Drive: --progress --progress-terminal-title --human-readable --filter-from Data/Labs/Lab_Banee/Misc/configurations-private/rclone/Blomp-Banee3-Gmail-Drive-filter.txt --dry-run
+
 echo "GDrive-Banee3"
 echo "----------------"
 echo "rclone sync --progress --progress-terminal-title --human-readable Data/Misc/Baneeishaque3-GDrive/ GDrive-Banee3: --fast-list --filter-from Data/Labs/Lab_Banee/rclone/FreeFileSync-filter.txt --dry-run"
 echo "----------------"
 rclone sync --progress --progress-terminal-title --human-readable Data/Misc/Baneeishaque3-GDrive/ GDrive-Banee3: --fast-list --filter-from Data/Labs/Lab_Banee/rclone/FreeFileSync-filter.txt --dry-run
+
 pause
+
 echo "GDrive-Banee4"
 echo "----------------"
 echo "rclone sync --progress --progress-terminal-title --human-readable Data/Misc/Baneeishaque4-GDrive/ GDrive-Banee4: --fast-list --filter-from Data/Labs/Lab_Banee/rclone/FreeFileSync-filter.txt --dry-run"
 echo "----------------"
 rclone sync --progress --progress-terminal-title --human-readable Data/Misc/Baneeishaque4-GDrive/ GDrive-Banee4: --fast-list --filter-from Data/Labs/Lab_Banee/rclone/FreeFileSync-filter.txt --dry-run
+
 pause
+
 echo "GDrive-Banee5"
 echo "----------------"
 echo "rclone sync --progress --progress-terminal-title --human-readable Data/Misc/Baneeishaque5-GDrive/ GDrive-Banee5: --fast-list --dry-run"
 echo "----------------"
 rclone sync --progress --progress-terminal-title --human-readable Data/Misc/Baneeishaque5-GDrive/ GDrive-Banee5: --fast-list --dry-run
 
+rclone sync --progress --progress-terminal-title --human-readable Data/Misc/Baneeishaque6-GDrive/ GDrive-Banee6: --fast-list --filter-from Data/Labs/Lab_Banee/Misc/configurations-private/rclone/GDrive-Banee6-root-filter.txt --dry-run
+
+rclone sync Data/Misc/Baneeishaque-GPhotos/album/ GPhotos-Banee:album/ --progress --progress-terminal-title --human-readable --dry-run
+
+rclone sync Data/Misc/Baneeishaque2-GPhotos/album/ GPhotos-Banee2:album/ --progress --progress-terminal-title --human-readable --dry-run
+
+rclone sync Data/Misc/Baneeishaque3-GPhotos/album/ GPhotos-Banee3:album/ --progress --progress-terminal-title --human-readable --dry-run
+
+rclone sync Data/Misc/Mega-Banee-Gmail-Drive/ Mega-Banee-Gmail: --progress --progress-terminal-title --human-readable --filter-from Data/Labs/Lab_Banee/Misc/configurations-private/rclone/Mega-Banee-Gmail-root-filter.txt --dry-run
+
+rclone sync Data/Misc/Mega-Banee2-Gmail-Drive/ Mega-Banee2-Gmail: --progress --progress-terminal-title --human-readable --filter-from Data/Labs/Lab_Banee/Misc/configurations-private/rclone/Mega-Banee2-Gmail-root-filter.txt --dry-run
+
+rclone sync Data/Misc/Mega-Banee3-Gmail-Drive/ Mega-Banee3-Gmail: --progress --progress-terminal-title --human-readable --filter-from Data/Labs/Lab_Banee/Misc/configurations-private/rclone/Mega-Banee3-Gmail-root-filter.txt --dry-run
+
+rclone sync Data/Misc/Mega-Banee4-Gmail-Drive/ Mega-Banee4-Gmail: --progress --progress-terminal-title --human-readable --filter-from Data/Labs/Lab_Banee/Misc/configurations-private/rclone/Mega-Banee4-Gmail-root-filter.txt --dry-run
+
+rclone sync Data/Misc/Mega-Banee5-Gmail-Drive/ Mega-Banee5-Gmail: --progress --progress-terminal-title --human-readable --filter-from Data/Labs/Lab_Banee/Misc/configurations-private/rclone/Mega-Banee4-Gmail-root-filter.txt --dry-run
+
+rclone sync Data/Misc/Mega-Banee6-Gmail-Drive/ Mega-Banee6-Gmail: --progress --progress-terminal-title --human-readable --dry-run
+
+rclone sync Data/Misc/Mega-Banee7-Gmail-Drive/ Mega-Banee7-Gmail: --progress --progress-terminal-title --human-readable --dry-run
+
+rclone sync Data/Misc/Miles-5GB-public_html/ Miles-5GB:public_html --progress --progress-terminal-title --human-readable --filter-from Data/Labs/Lab_Banee/Misc/configurations-private/rclone/Miles-5GB-public_html-filter.txt --dry-run
+
+rclone sync Data/Misc/One-Drive-Banee-Hotmail-Drive/ One-Drive-Banee-Hotmail: --progress --progress-terminal-title --human-readable --dry-run
+
+rclone sync Data/Misc/pCloud-Drive/ Pcloud-Banee-Gmail: --progress --progress-terminal-title --human-readable --dry-run
