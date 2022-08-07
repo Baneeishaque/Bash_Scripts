4a8c22b3962a5e991e48b73ebd40145ce69ead9d
Banee Ishaque K <baneeishaque@gmail.com>
2022-08-08 04:55:53 +0530

    Blomp3 Fix
    Signed-off-by: Banee Ishaque K <baneeishaque@gmail.com>

--- Rclone/rcloneMoveFolders.bash
+++ Rclone/rcloneMoveFolders.bash
@@ -1,13 +1,13 @@
-rclone lsf Blomp-Banee2-Gmail-Drive: | while read -r i; do
+rclone lsf Blomp-Banee3-Gmail-Drive: | while read -r i; do
     # echo "$i"
-    if [ "$i" != "Windows XP SP 2.ova" ]; then
+    if [ "$i" != "IE8.Win7.VirtualBox.zip" ] && [ "$i" != "VMware Images/" ]; then
         # echo "$i"
         i2=${i::-1}
         # echo "$i2"
-        rclone lsf Blomp-Banee2-Gmail-Drive:"$i2" | while read -r j; do
+        rclone lsf Blomp-Banee3-Gmail-Drive:"$i2" | while read -r j; do
             # echo "$j"
-            # rclone move "Blomp-Banee2-Gmail-Drive:$i2/$j" "Blomp-Banee2-Gmail-Drive:VMware Images/$i2 VMware/" --create-empty-src-dirs --delete-empty-src-dirs --dry-run --log-level DEBUG
-            rclone move "Blomp-Banee2-Gmail-Drive:$i2/$j" "Blomp-Banee2-Gmail-Drive:VMware Images/$i2 VMware/" --create-empty-src-dirs --delete-empty-src-dirs --log-level DEBUG
+            rclone move "Blomp-Banee3-Gmail-Drive:$i2/$j" "Blomp-Banee3-Gmail-Drive:VMware Images/$i2 VMware/" --create-empty-src-dirs --delete-empty-src-dirs --dry-run --log-level DEBUG
+            # rclone move "Blomp-Banee3-Gmail-Drive:$i2/$j" "Blomp-Banee3-Gmail-Drive:VMware Images/$i2 VMware/" --create-empty-src-dirs --delete-empty-src-dirs --log-level DEBUG
         done
     fi
 done
