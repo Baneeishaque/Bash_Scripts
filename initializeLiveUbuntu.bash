02bcb0ccfdc228138e7b613449e06060acc7d3b0
Banee Ishaque K <Baneeishaque@gmail.com>
2023-06-29 02:00:32 +0530

    Create initializeLiveUbuntu.bash

---
+++ initializeLiveUbuntu.bash
@@ -0,0 +1,1 @@
+glink="https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb" && wget $glink && sudo apt install ./google-chrome-stable_current_amd64.deb && rm $(basename $glink)
