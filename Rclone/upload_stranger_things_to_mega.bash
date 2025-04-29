2b05c6b4bdbd7719674b3d8a60de098561d65031
Banee Ishaque K <baneeishaque@gmail.com>
2025-04-29 08:35:46 +0530

    refactor: Enhance upload script for Stranger Things episodes by improving remote selection and free space management
    Signed-off-by: Banee Ishaque K <baneeishaque@gmail.com>

--- Rclone/upload_stranger_things_to_mega.bash
+++ Rclone/upload_stranger_things_to_mega.bash
@@ -1,14 +1,28 @@
-#!/bin/bash
+#!/opt/homebrew/bin/bash
 
-# List of Mega remotes
-mega_remotes=(
-    "Mega-Banee-Proton"
-    "Mega-Banee4-Gmail"
-    "Mega-Banee5-Gmail"
-    "Mega-Banee6-Gmail"
-    "Mega-Banee7-Gmail"
-    "Mega-Banee-Hotmail"
-)
+# Remote type preference order
+preferred_types=(drive onedrive yandex mailru pcloud mega)
+
+# Always source get_remote_free_space.bash
+script_dir="$(dirname "$0")"
+# shellcheck source=/dev/null
+source "$script_dir/get_remote_free_space.bash"
+
+# Get all remotes and their free space into an array using the function
+mapfile -t remote_lines < <(get_remote_free_space 1)
+
+# Parse remotes into associative arrays
+declare -A remote_type_map
+declare -A remote_free_map
+remote_list=()
+for line in "${remote_lines[@]}"; do
+    rname=$(echo "$line" | cut -d'|' -f1)
+    rtype=$(echo "$line" | cut -d'|' -f2)
+    rfree=$(echo "$line" | cut -d'|' -f3)
+    remote_type_map["$rname"]="$rtype"
+    remote_free_map["$rname"]="$rfree"
+    remote_list+=("$rname")
+done
 
 # Path to your Downloads directory
 downloads_dir="$HOME/Downloads"
@@ -16,11 +30,10 @@ downloads_dir="$HOME/Downloads"
 # Find all Stranger Things episodes in Downloads, sorted
 episodes=("$downloads_dir"/Stranger.Things.S04E*.mkv)
 
-# Check if the number of episodes matches the number of remotes
-if [ ${#episodes[@]} -gt ${#mega_remotes[@]} ]; then
-    echo "Warning: More episodes than Mega remotes. Some episodes will not be uploaded."
-    elif [ ${#episodes[@]} -lt ${#mega_remotes[@]} ]; then
-    echo "Warning: More Mega remotes than episodes. Some remotes will not be used."
+# Check if any episodes were found
+if [ ! -e "${episodes[0]}" ]; then
+    echo "No Stranger Things episodes found in $downloads_dir."
+    exit 1
 fi
 
 # Check if we should actually run or just dry run
@@ -33,16 +46,47 @@ else
     echo "Pass 'run' as an argument to actually upload."
 fi
 
-# Upload each episode to the corresponding Mega remote
-for i in "${!episodes[@]}"; do
-    episode="${episodes[$i]}"
-    remote="${mega_remotes[$i]}"
-    if [ -z "$remote" ]; then
-        echo "No more Mega remotes available for $episode"
+# Helper: get type preference index
+get_type_index() {
+    local type="$1"
+    for i in "${!preferred_types[@]}"; do
+        if [[ "${preferred_types[$i]}" == "$type" ]]; then
+            echo $i
+            return
+        fi
+    done
+    echo 99
+}
+
+# For each episode, pick the best remote, upload, then update free space
+for episode in "${episodes[@]}"; do
+    best_remote=""
+    best_type_index=99
+    best_free=-1
+    # Find best remote by type preference and free space
+    for rname in "${remote_list[@]}"; do
+        rtype="${remote_type_map[$rname]}"
+        rfree="${remote_free_map[$rname]}"
+        type_index=$(get_type_index "$rtype")
+        if [ "$type_index" -lt 99 ] && [ "$rfree" -ge 0 ]; then
+            if [ "$best_remote" = "" ] || [ "$type_index" -lt "$best_type_index" ] || { [ "$type_index" -eq "$best_type_index" ] && [ "$rfree" -gt "$best_free" ]; }; then
+                best_remote="$rname"
+                best_type_index="$type_index"
+                best_free="$rfree"
+            fi
+        fi
+    done
+    if [ -z "$best_remote" ]; then
+        echo "No suitable remote found for $episode"
         break
     fi
-    echo "Uploading '$episode' to '$remote:/' ..."
-    rclone copy "$episode" "$remote:/" --progress --stats-one-line $dry_run_flag
+    echo "Uploading '$episode' to '$best_remote:/' (type: ${remote_type_map[$best_remote]}, free: ${remote_free_map[$best_remote]} bytes) ..."
+    rclone copy "$episode" "$best_remote:/" --progress --stats-one-line $dry_run_flag
+    # Update in-memory free space for the chosen remote
+    if [ -f "$episode" ]; then
+        fsize=$(stat -f%z "$episode")
+        remote_free_map["$best_remote"]=$(( ${remote_free_map[$best_remote]} - fsize ))
+    fi
 done
 
 echo "Done."
