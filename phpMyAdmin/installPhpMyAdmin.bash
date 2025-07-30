8b033f9ff16dc89ec57574938320865d3c664335
Banee Ishaque K <baneeishaque@gmail.com>
2025-07-31 04:37:58 +0530

    feat(installPhpMyAdmin): Add script to install phpMyAdmin

---
+++ phpMyAdmin/installPhpMyAdmin.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+source ../brewFormulaInstallHelper.bash
+
+phpMyAdminInstallFolder=/opt/phpMyAdmin-english
+if [ ! -d $phpMyAdminInstallFolder ]; then
+    installBrewFormula pup
+    phpMyAdminDownloadUrl=$(wget -O - https://www.phpmyadmin.net/downloads | pup 'a.download_popup attr{href}' | grep --max-count=1 'english.zip')
+    wget $phpMyAdminDownloadUrl
+    phpMyAdminArchieveFile=$(basename $phpMyAdminDownloadUrl)
+    sudo unzip $phpMyAdminArchieveFile -d /opt/
+    rm $phpMyAdminArchieveFile
+    phpMyAdminFolder=$(echo $phpMyAdminArchieveFile | sed 's/\(.*\)\..*/\1/')
+    sudo mv /opt/$phpMyAdminFolder $phpMyAdminInstallFolder
+    sudo cp $phpMyAdminInstallFolder/config.sample.inc.php $phpMyAdminInstallFolder/config.inc.php
+    printf "\n\$cfg['AllowArbitraryServer'] = true;" | sudo tee -a $phpMyAdminInstallFolder/config.inc.php >/dev/null
+fi
---
+++ phpMyAdmin/runPhpMyAdmin.bash
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+./installPhpMyAdmin.bash
+cd $phpMyAdminInstallFolder
+sudo php -S 0.0.0.0:80
