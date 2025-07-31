8adb57958ccf0c72419d21d6c255d2f2f35caf1d
Banee Ishaque K <baneeishaque@gmail.com>
2025-07-31 15:17:49 +0530

    fix(installNgrok): Correctly source updatePackageIndex.bash

--- installNgrok.bash
+++ installNgrok.bash
@@ -2,5 +2,6 @@
 
 curl -sSL https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null
 echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list >/dev/null
-./updatePackageIndex.bash
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+source "$SCRIPT_DIR/updatePackageIndex.bash"
 sudo apt install -y ngrok
