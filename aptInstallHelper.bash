78208a86289bdd32e9a6c04243afb1a00eba9448
Banee Ishaque K <baneeishaque@gmail.com>
2025-08-01 19:43:50 +0530

    feat: aptInstallHelper

---
+++ aptInstallHelper.bash
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Function to install packages using apt
+# Args:
+#   $1: Package name(s) to install (space-separated)
+# Returns:
+#   0: Installation successful
+#   1: Installation failed
+
+aptInstall() {
+
+    SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+    source "$SCRIPT_DIR/addToBashConfigurationHelper.bash"
+
+  if [ -z "$1" ]; then
+    echo "Error: No package name(s) provided."
+    return 1
+  fi
+
+  echo "Updating package index..."
+  ./updatePackageIndex.bash
+
+  echo "Installing package(s): $1"
+  sudo apt install -y $1
+
+  if [ $? -eq 0 ]; then
+    echo "Package(s) $1 installed successfully."
+    return 0
+  else
+    echo "Error: Failed to install package(s) $1"
+    return 1
+  fi
+}
