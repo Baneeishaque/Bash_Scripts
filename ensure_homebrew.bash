2020bd75e115fa991cdfd998db84f246343ad878
Banee Ishaque K <baneeishaque@gmail.com>
2025-08-01 19:43:05 +0530

    feat: install HomeBrew enhancements

--- ensure_homebrew.bash
+++ ensure_homebrew.bash
@@ -1,8 +1,15 @@
 #!/bin/bash
 
 ensure_homebrew() {
+
+    SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+    source "$SCRIPT_DIR/addToBashConfigurationHelper.bash"
+
     if ! command -v brew &>/dev/null; then
         echo "Homebrew not found. Installing Homebrew..."
-        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+        bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+        BREW_EVAL_CMD="$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+        addToBashConfiguration "eval $BREW_EVAL_CMD"
+        eval "$BREW_EVAL_CMD"
     fi
 }
