54d7c33ca0c2157d93f5a6b89684f59101b34918
Banee Ishaque K <baneeishaque@gmail.com>
2025-08-09 21:35:53 +0530

    refactor(setupFlutterViaFVM): simplify Java installation logic

--- gitpod_related_scripts/setupFlutterViaFVM.bash
+++ gitpod_related_scripts/setupFlutterViaFVM.bash
@@ -40,85 +40,8 @@ else
     fvm install "$user_version"
 fi
 
-gradleVersion=$(grep 'distributionUrl' \
-  "fvm/versions/$user_version/examples/hello_world/android/gradle/wrapper/gradle-wrapper.properties" \
-  | cut -d'=' -f2 | cut -d'-' -f2 | tr -d '*')
-echo "Gradle Version: $gradleVersion"
-
-echo "Fetching Gradle-Java compatibility information…"
-html_content=$(curl -s https://docs.gradle.org/current/userguide/compatibility.html)
-
-declare -A compile_gradle_map runtime_gradle_map
-
-while IFS=$'\t' read -r jdk compile runtime; do
-  compile_gradle_map["$jdk"]=$compile
-  runtime_gradle_map["$jdk"]=$runtime
-done < <(
-  echo "$html_content" \
-    | pup 'div#content div.sect1 > h2#java_runtime + div.sectionbody table.tableblock tbody tr json{}' \
-    | jq -r '
-        .[] |
-        .children[0].children[0].text as $jdk |
-        # take only the first token, strip "*" or trailing "."
-        (.children[1].children[0].text
-           | split(" ")[0]
-           | sub("\\*";""))                as $compile |
-        (.children[2].children[0].text
-           | split(" ")[0]
-           | sub("\\*";"")
-           | sub("\\.$";""))               as $runtime |
-        "\($jdk)\t\($compile)\t\($runtime)"
-      '
-)
-
-if (( ${#compile_gradle_map[@]} == 0 )); then
-  echo "ERROR: Could not parse Java-Gradle table!"
-  exit 1
-fi
-
-sorted_jdks=( $(printf "%s\n" "${!compile_gradle_map[@]}" | sort -V) )
-
-compile_jdk=""
-run_jdk=""
-for jdk in "${sorted_jdks[@]}"; do
-  cv=${compile_gradle_map[$jdk]}
-  rv=${runtime_gradle_map[$jdk]}
-
-  if [[ "$cv" != "N/A" ]] \
-    && dpkg --compare-versions "$gradleVersion" ge "$cv"; then
-    compile_jdk="$jdk"
-  fi
-
-  if [[ "$rv" != "N/A" ]] \
-    && dpkg --compare-versions "$gradleVersion" ge "$rv"; then
-    run_jdk="$jdk"
-  fi
-done
-
-echo
-echo "Java ↔ Gradle Compatibility Table:"
-for jdk in "${sorted_jdks[@]}"; do
-  echo "  Java $jdk → compile w/ Gradle ${compile_gradle_map[$jdk]}, run w/ Gradle ${runtime_gradle_map[$jdk]}"
-done
-
-echo
-echo "For Gradle $gradleVersion:"
-echo "  • Minimum Java to compile your build: $compile_jdk"
-echo "  • Minimum Java to run Gradle itself:  $run_jdk"
-
-. $DIR/../installMise.bash
-
-echo "Installing Java $compile_jdk and Java $run_jdk via mise…"
-mise install java@"$compile_jdk"
-mise install java@"$run_jdk"
-
-echo
-echo "Check compile-JDK version directly:"
-mise exec java@"$compile_jdk" -- java --version
-
-echo
-echo "Check run-JDK version directly:"
-mise exec java@"$run_jdk" -- java --version
+source "$DIR/../gradle_java_compatibility.bash"
+install_jdks_based_on_gradle_version "" "fvm/versions/$user_version/examples/hello_world/android/gradle/wrapper/gradle-wrapper.properties"
 
 . $DIR/installAndroidSdk.bash
 
