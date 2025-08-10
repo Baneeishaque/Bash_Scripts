182a625fc6c167a8818d98490bfe8772639c86d0
Banee Ishaque K <baneeishaque@gmail.com>
2025-08-10 21:10:57 +0530

    fix(aptInstall): add missing libxss1 dependency

--- install_google_chrome_ubuntu_dependencies.bash
+++ install_google_chrome_ubuntu_dependencies.bash
@@ -7,7 +7,7 @@ install_google_chrome_ubuntu_dependencies() {
     SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
     source "$SCRIPT_DIR/aptInstallHelper.bash"
 
-    aptInstall "libasound2-dev libgtk-3-dev libnss3-dev fonts-noto fonts-noto-cjk"
+    aptInstall "libasound2-dev libgtk-3-dev libnss3-dev fonts-noto fonts-noto-cjk libxss1"
 }
 
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
