61c9ce60e0045f1697301060a96c24471bbeec98
Banee Ishaque K <baneeishaque@gmail.com>
2025-08-10 19:28:47 +0530

    feat: Add install_deno script for Deno installation

---
+++ install_deno.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+instsll_deno() {
+
+    local SCRIPT_DIR
+
+    SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+    source "$SCRIPT_DIR/brewFormulaInstallHelper.bash"
+
+    installBrewFormula deno
+}
+
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    instsll_deno
+fi
