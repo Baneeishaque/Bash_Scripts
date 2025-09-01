440bb964d3d337e106d28ac15360bea6449ce387
Banee Ishaque K <baneeishaque@gmail.com>
2025-09-01 13:53:32 +0530

    Adds symlink for glab executable
    Creates a symbolic link for the glab executable in /usr/local/bin to ensure it is accessible in the system's PATH. This allows the GitLab CLI to be easily invoked from any directory.

--- installGitLabCli.bash
+++ installGitLabCli.bash
@@ -4,3 +4,5 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "$SCRIPT_DIR/brewFormulaInstallHelper.bash"
 
 installBrewFormula glab
+
+sudo ln -s /home/linuxbrew/.linuxbrew/bin/glab /usr/local/bin/glab
